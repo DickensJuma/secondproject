178d19b9e8ec7c6e7d8ec8e1f217e93327a13587
Cyrus Kiprop <cyruskiprop254@gmail.com>
2019-04-26 18:56:57 +0300

    a function to validate a parenthese

---
+++ validParenthese.js
@@ -0,0 +1,26 @@
+function validParentheses(parens){
+    var n = 0;
+    for (var i = 0; i < parens.length; i++) {
+      if (parens[i] == '(') n++;
+      if (parens[i] == ')') n--;
+      if (n < 0) return false;
+    }
+    
+    return n == 0;
+  }
+
+  function validParentheses(parens){
+    var stack = [],
+      i;
+    
+    for (i = 0; i < parens.length; i++) {
+      if (parens[i] === '(') {
+        stack.push(parens[i]);
+      }
+      else if('(' !== stack.pop()) {
+        return false;
+      }
+    }
+    
+    return stack.length === 0;
+  }
