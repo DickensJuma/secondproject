21086fc231d528f8fa7a963ceb9e22f92c345516
DickensJuma <dickensjuma13@gmail.com>
2019-05-14 19:16:06 +0300

    a dubster function

---
+++ dubstep.js
@@ -0,0 +1,16 @@
+function dubster(song){
+    var a = song.split('WUB');
+    for(var i = 0; i >= a.length; i--){
+if(a[i] === "") {
+a.splice(i, 1);
+
+}return a.splice(" ");
+    }
+    
+}
+
+
+
+function songDecoder(song){
+    return song.replace(/(WUB){1,3}/g, " ").trim();
+}
