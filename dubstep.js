7c8a9c7d8bc28edc3a820136abe07b52e004f306
DickensJuma <dickensjuma13@gmail.com>
2019-08-15 23:40:13 +0300

    restructure

--- dubstep.js
+++ dubstep.js
@@ -1,16 +1,21 @@
 function dubster(song){
 var a = song.split('WUB');
+
+
 for(var i = 0; i >= a.length; i--){
 if(a[i] === "") {
 a.splice(i, 1);
 
-}return a.splice(" ");
+}return a.splice(" ").join();
     }
     
 }
 
+console.log(dubster('IWUBLOVEWUBYOU'));
 
 
 function songDecoder(song){
     return song.replace(/(WUB){1,3}/g, " ").trim();
 }
+
+console.log(songDecoder('IWUBLOVEWUBYOU'));
